9e8103a2837d7cbb7c243ac69ad9760abbc482aa
gregmall <gregmall157@gmail.com>
2026-07-03 10:20:16 -0700

    fixed build error

--- app/page.js
+++ app/page.js
@@ -2,12 +2,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="App">
+      
       <header className="App-header">
         <img 
           src="/assets/logo.png" 
           style={{maxWidth: '80vw'}} />
+
       </header>
     </div>
   );
-  
+
 }
