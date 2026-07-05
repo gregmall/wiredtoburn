f122764151b35d7390c3703c39dae6a6375bcc86
gregmall <gregmall157@gmail.com>
2026-07-05 16:38:55 -0700

    changing layout

--- app/page.js
+++ app/page.js
@@ -2,12 +2,20 @@ export default function Home() {
   return (
     <div className="App">
 
-      <header className="App-header">
+      <header className="fixed top-0 w-full left-0">
         <img 
           src="/assets/wired.gif" 
           style={{maxWidth: '80vw'}} />
 
       </header>
+      <div className="flex">
+        {/* <img src="/circle1.jpg" style={{borderRadius: "50%", width: "30vw"}} /> */}
+        <img src="/circle2.jpg" style={{borderRadius: "50%", width: "30vw", margin: "10px"}} />
+        <img src="/circle3.jpg" style={{borderRadius: "50%", width: "30vw", margin: "10px"}} />
+        <img src="/circle4.jpg" style={{borderRadius: "50%", width: "30vw", margin: "10px"}} />
+        {/* <img src="/circle5.jpg" style={{borderRadius: "50%", width: "30vw"}} /> */}
+
+      </div>
     </div>
   );
 
