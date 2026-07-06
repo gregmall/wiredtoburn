4c90cdcb1972b0359d1b7b15b5e612fc54dc5f93
gregmall <gregmall157@gmail.com>
2026-07-06 15:28:33 -0700

    google verifying

--- app/layout.jsx
+++ app/layout.jsx
@@ -17,6 +17,9 @@ export const viewport = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="ZSwLCovjiW_aaTKiw5IiqCRLEE76HMrHyD5Os4CMSac" />
+      </head>
       <body>{children}</body>
     </html>
   );
